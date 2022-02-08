cc9aba967204db89d440bc8cc488bd35b1d7d445
Cecilia B <66726969+ceciliaB97@users.noreply.github.com>
2022-02-08 01:13:27 -0300

    agregar formularios simples SIN FUNCIONAMIENTO

--- envios/src/components/Login/Login.jsx
+++ envios/src/components/Login/Login.jsx
@@ -1,11 +1,53 @@
-import "./Login.css";
+import {appStyle, formStyle, labelStyle, inputStyle, submitStyle} from "./LoginStyle";
 
-const LoginContent = () => {
+import React from "react";
+// import ReactDOM from "react-dom";
+
+const Field = React.forwardRef(({ label, type }, ref) => {
   return (
-    <div class="loginScreen">
-        This is the login screen.
+    <div>
+      <label style={labelStyle}>{label}</label>
+      <input ref={ref} type={type} style={inputStyle} />
     </div>
   );
+});
+
+const Form = ({ onSubmit }) => {
+  const usernameRef = React.useRef();
+  const passwordRef = React.useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+    };
+    onSubmit(data);
+  };
+  return (
+    <form style={formStyle} onSubmit={handleSubmit}>
+      <Field ref={usernameRef} label="Username:" type="text" />
+      <Field ref={passwordRef} label="Password:" type="password" />
+      <div>
+        <button style={submitStyle} type="Submit">
+          Login
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const LoginContent = () => {
+   const handleSubmit = data => {
+        const json = JSON.stringify(data, null, 4);
+        console.clear();
+        console.log(json);
+    };
+    return (
+      <div style={appStyle}>
+        <Form onSubmit={handleSubmit} />
+      </div>
+    );
+  // return <div class="loginScreen">This is the login screen.</div>;
 };
 
 export default LoginContent;
--- envios/src/components/Register/Register.jsx
+++ envios/src/components/Register/Register.jsx
@@ -1,11 +1,54 @@
-import "./Register.css";
+import {appStyle, formStyle, labelStyle, inputStyle, submitStyle} from "./RegisterStyle";
 
-const RegisterContent = () => {
+import React from "react";
+// import ReactDOM from "react-dom";
+
+const Field = React.forwardRef(({ label, type }, ref) => {
   return (
-    <div class="registerScreen">
-        This is the register screen.
+    <div>
+      <label style={labelStyle}>{label}</label>
+      <input ref={ref} type={type} style={inputStyle} />
     </div>
   );
+});
+
+const Form = ({ onSubmit }) => {
+  const usernameRef = React.useRef();
+  const passwordRef = React.useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+    };
+    onSubmit(data);
+  };
+  return (
+    <form style={formStyle} onSubmit={handleSubmit}>
+      <Field ref={usernameRef} label="Username:" type="text" />
+      <Field ref={passwordRef} label="Password:" type="password" />
+      <Field ref={passwordRef} label="Password:" type="password" />
+      <div>
+        <button style={submitStyle} type="submit">
+          Register
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const RegisterContent = () => {
+   const handleSubmit = data => {
+        const json = JSON.stringify(data, null, 4);
+        console.clear();
+        console.log(json);
+    };
+    return (
+      <div style={appStyle}>
+        <Form onSubmit={handleSubmit} />
+      </div>
+    );
+  // return <div class="loginScreen">This is the login screen.</div>;
 };
 
 export default RegisterContent;
