9d6b03a42790bd3ea32db199a4ddeb50dc3ee43e
Cecilia B <66726969+ceciliaB97@users.noreply.github.com>
2022-02-08 14:09:41 -0300

    change minor things being warned

--- envios/src/components/App/App.js
+++ envios/src/components/App/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import LoginContent from "../Login";
-import RegisterContent from "../Register";
-import Dashboard from "../Dashboard";
+// import RegisterContent from "../Register";
+// import Dashboard from "../Dashboard";
 import React from "react";
 
 function App() {
